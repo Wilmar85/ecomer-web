55241dd431596a4b9a75d622ad23d29f9165ab39
wilmarg85 <wguevarac85@gmail.com>
2025-04-12 07:21:44 -0500

    se elimina la opcion eliminar cuenta y se agrega boton direccionando a mis pedidos

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -7,6 +7,14 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <!-- Botón Ver Pedidos -->
+            <div class="text-right mb-4">
+                <a href="{{ route('orders.index') }}" 
+                   class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    {{ __('Ver Pedidos') }}
+                </a>
+            </div>
+
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
@@ -16,12 +24,6 @@
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
-                </div>
-            </div>
-
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
                 </div>
             </div>
 
